06d379700b5ebd4b2eea67351e6b128bd38bb65c
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-04-09 16:29:05 -0300

    theoretically a lower end config?

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -127,6 +127,7 @@ application {
             "-XX:MaxGCPauseMillis=200",
             "-XX:MaxMetaspaceSize=512m",
             "-XX:ReservedCodeCacheSize=256m",
+            "-XX:ParallelGCThreads=8",
         )
     mainClass.set(MainClass)
 }
