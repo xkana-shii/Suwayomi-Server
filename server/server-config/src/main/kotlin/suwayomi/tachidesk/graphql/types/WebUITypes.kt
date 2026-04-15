8b4857c7cd35b439618c18109b95c2879784c326
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-04-15 16:53:39 -0300

    Update WebUITypes.kt

--- server/server-config/src/main/kotlin/suwayomi/tachidesk/graphql/types/WebUITypes.kt
+++ server/server-config/src/main/kotlin/suwayomi/tachidesk/graphql/types/WebUITypes.kt
@@ -27,9 +27,9 @@ enum class WebUIFlavor(
 ) {
     WEBUI(
         "WebUI",
-        "https://github.com/Suwayomi/Suwayomi-WebUI-preview",
-        "https://raw.githubusercontent.com/Suwayomi/Suwayomi-WebUI/master/versionToServerVersionMapping.json",
-        "https://api.github.com/repos/Suwayomi/Suwayomi-WebUI-preview/releases/latest",
+        "https://github.com/xkana-shii/Suwayomi-WebUI-preview",
+        "https://raw.githubusercontent.com/xkana-shii/Suwayomi-WebUI/master/versionToServerVersionMapping.json",
+        "https://api.github.com/repos/xkana-shii/Suwayomi-WebUI-preview/releases/latest",
         "Suwayomi-WebUI",
     ),
 
