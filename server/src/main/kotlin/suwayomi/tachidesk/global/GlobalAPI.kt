aa37fde6a8228bd1e77f9202fdfc0ddfac819abe
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-04-21 11:43:38 -0300

    test

--- server/src/main/kotlin/suwayomi/tachidesk/global/GlobalAPI.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/global/GlobalAPI.kt
@@ -10,6 +10,7 @@ package suwayomi.tachidesk.global
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.patch
 import io.javalin.apibuilder.ApiBuilder.path
+import io.javalin.apibuilder.ApiBuilder.post
 import io.javalin.apibuilder.ApiBuilder.ws
 import suwayomi.tachidesk.global.controller.GlobalMetaController
 import suwayomi.tachidesk.global.controller.SettingsController
@@ -24,6 +25,7 @@ object GlobalAPI {
         path("settings") {
             get("about", SettingsController.about)
             get("check-update", SettingsController.checkUpdate)
+            post("shutdown", SettingsController.shutdown)
         }
         path("webview") {
             get("", WebViewController.webview)
